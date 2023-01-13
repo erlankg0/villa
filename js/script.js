e8e918537d1035dc369253445a6336869b6d4678
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2023-01-13 23:47:05 +0300

    modual window

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-document.querySelectorAll('accordion__title').forEach(item =>  {
-    item.addEventListener('click', () => {
-        // get accordion__content
-        const content = item.nextElementSibling;
-        // toggle accordion__content
-        console.log(content);,
-        console.log(item);
-    })
-    }
-)
+// play video on click of video tag in the page 
+
+$(document).ready(function () {
+    var video = document.getElementById("video-player");
+    video.addEventListener("click", function () {
+        if (video.paused) {
+            video.play();
+            // autoplay video
+            video.controls = true;
+        } else {
+            video.pause();
+            video.controls = false;
+        }
+    });
+});
+
+
+const openPopup = document.getElementById("poster");
+const close = document.getElementById("close");
+const popUp = document.getElementById('pop_up');
+const video = document.getElementById("video-player");
+
+openPopup.addEventListener('click', function (e) {
+    console.log("open");
+    e.preventDefault();
+    popUp.style.display = "block";
+    video.controls = true;
+});
+
+close.addEventListener('click', function (e) {
+    e.preventDefault();
+    popUp.style.display = "none";
+    video.pause();
+    video.controls = false;
+});
